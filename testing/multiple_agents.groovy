ce3b7b9a3f7298877a07d6ece276996425c90845
Petri Sadinmäki <60505390+qubilea-sadinmaki@users.noreply.github.com>
2020-05-22 10:48:53 +0300

    Create multiple_agents.groovy

---
+++ testing/multiple_agents.groovy
@@ -0,0 +1,32 @@
+#!/usr/bin/env groovy
+
+stage('Build') {
+    node {
+        checkout scm
+        sh 'make'
+        stash includes: '**/target/*.jar', name: 'app' 
+    }
+}
+
+stage('Test') {
+    node('linux') { 
+        checkout scm
+        try {
+            unstash 'app' 
+            sh 'make check'
+        }
+        finally {
+            junit '**/target/*.xml'
+        }
+    }
+    node('windows') {
+        checkout scm
+        try {
+            unstash 'app'
+            bat 'make check' 
+        }
+        finally {
+            junit '**/target/*.xml'
+        }
+    }
+}
